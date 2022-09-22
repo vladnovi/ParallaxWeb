e36489f14598ba286f28ceae34c31aff253a7f26
vladnovi <vlad_novi@bk.ru>
2022-09-22 20:55:56 +0300

    Add another parallax script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+new Rellax('.rellax');
 
+new Parallax(document.getElementById('scene_1'));
+new Parallax(document.getElementById('scene_2'));
+new Parallax(document.getElementById('scene_3'));
+new Parallax(document.getElementById('scene_4'));
+new Parallax(document.getElementById('scene_5'));
+new Parallax(document.getElementById('scene_6'));
+new Parallax(document.getElementById('scene_7'));
+new Parallax(document.getElementById('scene_8'));
+new Parallax(document.getElementById('scene_9'));
+new Parallax(document.getElementById('scene_10'));
+new Parallax(document.getElementById('scene_11'));
+new Parallax(document.getElementById('scene_12'));
+new Parallax(document.getElementById('scene_13'));
+new Parallax(document.getElementById('scene_14'));
+new Parallax(document.getElementById('scene_15'));
